f16b063c020e5ab4983994164249217ec5aecf32
mhtf19 <mhtf19@gmail.com>
2020-11-19 17:57:26 -0500

    First part of wordgame stuff

---
+++ Assets/__Scripts/WordGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public enum GameMode {
+    preGame, //before game
+    loading,
+    makeLevel,
+    levelPrep,
+    inLevel
+}
+
+public class WordGame : MonoBehaviour
+{
+    static public WordGame S;
+
+    [Header("Set Dynamically")]
+    public GameMode mode = GameMode.preGame;
+
+    void Awake(){
+        S = this;
+    }
+    // Start is called before the first frame update
+    void Start(){
+        mode = GameMode.loading;
+
+        WordList.INIT();
+    }
+
+    public void WordListParseComplete(){
+        mode = GameMode.makeLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
