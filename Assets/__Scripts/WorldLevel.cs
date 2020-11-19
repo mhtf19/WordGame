aa6547304e098c8bf26bf45c3b9c01c5e3d147af
mhtf19 <mhtf19@gmail.com>
2020-11-19 18:10:27 -0500

    part 1 of World level script

---
+++ Assets/__Scripts/WorldLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WorldLevel{
+
+    public int levelNum;
+    public int longWordIndex;
+    public string word;
+
+    public Dictionary<char, int> charDict;
+
+    public List<string> subWords;
+
+    static public Dictionary<char, int> MakeCharDict(string w){
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        char c;
+        for(int i = 0; i<w.Length; i++){
+            c = w[i];
+            if(dict.ContainsKey(c)){
+                dict[c]++;
+            } else{
+                dict.Add (c, 1);
+            }
+        }
+        return(dict);
+    }
+}
