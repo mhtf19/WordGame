531ab562da47720a337bde4336119a2d3e100d5e
mhtf19 <mhtf19@gmail.com>
2020-11-20 00:30:33 -0500

    Letter class up to pg773

--- Assets/__Scripts/Letter.cs
+++ Assets/__Scripts/Letter.cs
@@ -4,6 +4,55 @@ using UnityEngine;
 
 public class Letter : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public TextMesh tMesh;
+    public Renderer tRend;
+
+    public bool big = false;
+
+    private char _c;  //the char shown on letter
+    private Renderer rend;
+
+    void Awake(){
+        tMesh = GetComponentInChildren<TextMesh>();
+        tRend = tMesh.GetComponent<Renderer>();
+        rend = GetComponent<Renderer>();
+        visible = false;
+    }
+
+    //set or get _c and letter shown by 3d text
+    public char c{
+        get{return _c;}
+        set{
+            _c = value;
+            tMesh.text = _c.ToString();
+        }
+    }
+
+    public string str{
+        get{return _c.ToString();}
+        set {c = value[0];}
+    }
+
+    //changes mode of renderer- char visible or not
+
+    public bool visible{
+        get{return tRend.enabled;}
+        set{tRend.enabled = value;}
+    }
+
+    public Color color {
+        get {return rend.material.color;}
+        set {rend.material.color=value;}
+    }
+
+    public Vector3 pos{
+        set{
+            transform.position = value;
+            //more added later
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
