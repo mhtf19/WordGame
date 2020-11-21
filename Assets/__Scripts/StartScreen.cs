f140a6f6ebdbd6ed821d4e201e0f0e0fa8755b45
mhtf19 <mhtf19@gmail.com>
2020-11-20 19:50:17 -0500

    doot

--- Assets/__Scripts/StartScreen.cs
+++ Assets/__Scripts/StartScreen.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPrefs.SetInt("CurrentScore", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void On7Button(){
+        PlayerPrefs.SetInt("GameType", 7);
+        SceneManager.LoadScene("Game");       
+    }
+
+    void On6Button(){
+        PlayerPrefs.SetInt("GameType", 6);
+        SceneManager.LoadScene("Game");
     }
 }
