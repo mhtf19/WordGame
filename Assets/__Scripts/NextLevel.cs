8e6e193002ac68c84117b8cd69602796c1a1ed6f
mhtf19 <mhtf19@gmail.com>
2020-11-20 20:37:58 -0500

    doot

--- Assets/__Scripts/NextLevel.cs
+++ Assets/__Scripts/NextLevel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NextLevelOrQuit : MonoBehaviour
+public class NextLevel : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
